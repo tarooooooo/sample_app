f64e7cdebe1b6051dbe8fedc82245edb00b94794
tarooooooo <ryutaromaeda83@gmail.com>
2021-07-22 06:30:45 +0000

    [Finish]08

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,11 @@
-const { environment } = require
+const { environment } = require('@rails/webpacker')
 
-
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+)
 
 module.exports = environment
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get login_
+    get login_path
     assert_response :success
   end
 
